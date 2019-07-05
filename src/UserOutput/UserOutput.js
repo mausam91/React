9829ce81af442b5374f8e9961157689aa6cc87ea
PINAKIN PATEL <PINAKIN@pinakins-mbp.gateway.sasktel.net>
2019-07-05 17:42:52 -0600

    Added Files

---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './UserOutput.css'
+
+const userOutput = (props) => {
+    return ( <
+        div className = "UserOutput" > < p > Username: { props.userName } < /p> <p> HI </p > < /div>
+    );
+};
+
+export default userOutput;
